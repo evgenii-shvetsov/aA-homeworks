0205b1a58a347f7e3a79a890d1b234ad8c2e99a8
evgenii-shvetsov <sea.dev91@gmail.com>
2022-10-18 21:25:15 -0700

    add_numbers done

--- recursion_homework/recursion.rb
+++ recursion_homework/recursion.rb
@@ -1,7 +1,10 @@
+require "byebug"
+
 # Exercise 1 - sum_to
 
 # Write a function sum_to(n) that uses recursion to calculate the sum from 1 to n (inclusive of n).
-def sum_to(n)
+
+def sum_to(n)  #DONE
     return nil if n <= 0
     return 1 if n == 1
     
@@ -13,3 +16,21 @@ end
 # p  sum_to(1)  # => returns 1
 # p   sum_to(9)  # => returns 45
 # p   sum_to(-8)  # => returns nil
+
+# Exercise 2 - add_numbers
+
+# Write a function add_numbers(nums_array) that takes in an array of Integers and returns the sum of those numbers. Write this method recursively.
+
+def add_numbers(arr) #DONE
+    return nil if arr.empty?
+    return arr[0] if arr.length <= 1
+    # debugger
+    arr[0] + add_numbers(arr[1..-1])
+end
+
+  # Test Cases
+# p  add_numbers([1,2,3,4]) # => returns 10
+# p  add_numbers([3]) # => returns 3
+# p  add_numbers([-80,34,7]) # => returns -39
+# p  add_numbers([]) # => returns nil
+
