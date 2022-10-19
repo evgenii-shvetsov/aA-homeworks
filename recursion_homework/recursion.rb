a2b2c27e0ec903c70891ebf889a0f43ed41384fd
evgenii-shvetsov <sea.dev91@gmail.com>
2022-10-18 21:52:31 -0700

    ice_cream done

--- recursion_homework/recursion.rb
+++ recursion_homework/recursion.rb
@@ -47,6 +47,30 @@ end
 #   gamma_fnc(8)  # => returns 5040
 
 
+# Exercise 4 - Ice Cream Shop
+
+# Write a function ice_cream_shop(flavors, favorite) that takes in an array of ice cream flavors available at the ice cream shop, as well as the user's favorite ice cream flavor. Recursively find out whether or not the shop offers their favorite flavor.
+
+def ice_cream_shop(flavors, favorite)
+    return nil if flavors.empty?
+    
+    return true if flavors[0] == favorite
+    
+    return false if flavors.length <= 1
+
+    ice_cream_shop(flavors[1..-1],favorite)
+   
+end
+
+# Test Cases
+p ice_cream_shop(['vanilla', 'strawberry'], 'blue moon')  # => returns false
+p ice_cream_shop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')  # => returns true
+p ice_cream_shop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')  # => returns false
+ice_cream_shop(['moose tracks'], 'moose tracks')  # => returns true
+p ice_cream_shop([], 'honey lavender')  # => returns false
+
+
+
 # Exercise 5 - Reverse
 
 # Write a function reverse(string) that takes in a string and returns it reversed.
