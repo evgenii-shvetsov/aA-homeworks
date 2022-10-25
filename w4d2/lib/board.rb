13dedf5a6fd5f87069f92b9158e999a203a0b911
evgenii-shvetsov <sea.dev91@gmail.com>
2022-10-24 23:01:10 -0700

    Finish project

--- w4d2/lib/board.rb
+++ w4d2/lib/board.rb
@@ -53,6 +53,16 @@ class Board
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+        #choose new place to start on your side
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      # if ended on empty cup, we switch players
+       :switch
+    else
+      ending_cup_idx
+    end
+
   end
 
   def render
