9de8b61bd160fba44c1a98f21305ea8a0b96aa07
evgenii-shvetsov <sea.dev91@gmail.com>
2022-11-27 19:31:42 -0800

    set up canvas element

--- w9d1/canvas_practice/practice.js
+++ w9d1/canvas_practice/practice.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(){
+    const canvas = document.getElementById("mycanvas");
+    canvas.width = 500;
+    canvas.height = 500;
+
+    const ctx = canvas.getContext("2d");
+
 
 });
