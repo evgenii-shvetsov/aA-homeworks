0218ee1099000cc0ce81e124d91dbfbc8415806d
evgenii-shvetsov <sea.dev91@gmail.com>
2022-11-30 20:53:13 -0800

    complete weather api ajax request

--- w9d4/practice-for-ch-js-first-ajax/index.js
+++ w9d4/practice-for-ch-js-first-ajax/index.js
@@ -1,3 +1,27 @@
 console.log("Hello from the console!");
 
 // Your code here
+
+
+
+const id = "bcb83c4b54aee8418983c2aff3073b3b";
+const city = "London,UK";
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${id}`)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(response);
+    }
+  })
+  .then(data => {
+    console.log("Success!");
+    console.log(data);
+  })
+  .catch(errorResponse => {
+    console.log("Failure!");
+    console.error(errorResponse);
+  });
+
+  console.log("The AJAX request has been dispatched.")
