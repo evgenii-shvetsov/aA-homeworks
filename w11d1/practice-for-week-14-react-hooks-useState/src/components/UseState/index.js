5e72b798e586da751dd0921599edd7ed9d90e4e3
evgenii-shvetsov <sea.dev91@gmail.com>
2022-12-10 16:48:13 -0800

    add div wrapper

--- w11d1/practice-for-week-14-react-hooks-useState/src/components/UseState/index.js
+++ w11d1/practice-for-week-14-react-hooks-useState/src/components/UseState/index.js
@@ -2,13 +2,13 @@ import { useState } from "react";
 import "./UseState.css";
 
 const UseState = () => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("state light");
   const [count, setCount] = useState(0);
 
   return (
     <div className={theme}>
       <h1>UseState Component</h1>
-      <button onClick={() => setTheme(theme == "dark" ? "light": "dark")}>Toggle theme</button>
+      <button onClick={() => setTheme(theme == "state dark" ? "state light": "state dark")}>Toggle theme</button>
       
       <h1>{count}</h1>
       <button onClick={()=>setCount((prevCount) => prevCount + 1)}>Increment</button>
