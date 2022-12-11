fbda7b0f160f8e4242c9e55112013b5b0e670ea7
evgenii-shvetsov <sea.dev91@gmail.com>
2022-12-10 16:01:44 -0800

    add useStateHook

--- w11d1/practice-for-week-14-react-hooks-useState/src/components/UseState/index.js
+++ w11d1/practice-for-week-14-react-hooks-useState/src/components/UseState/index.js
@@ -1,7 +1,14 @@
+import { useState } from "react";
+import "./UseState.css";
+
 const UseState = () => {
+  const [theme, setTheme] = useState("light");
+
   return (
-    <div>
+    <div className={theme}>
       <h1>UseState Component</h1>
+      <button onClick={() => setTheme("dark")}>Dark</button>
+      <button onClick={() => setTheme("light")}>Light</button>
     </div>
   );
 };
