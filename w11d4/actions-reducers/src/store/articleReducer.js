ae4b063e2b9a00b2d4daded181df5617dfb41410
evgenii-shvetsov <sea.dev91@gmail.com>
2022-12-14 22:34:28 -0800

    add reducer

--- w11d4/actions-reducers/src/store/articleReducer.js
+++ w11d4/actions-reducers/src/store/articleReducer.js
@@ -8,3 +8,17 @@ export const loadArticles = ()=>{
         articles
     }
 }
+
+
+const initialState = { entries: [], isLoading: true };
+
+const articleReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOAD_ARTICLES:
+      return { ...state, entries: [...action.articles] };
+    default:
+      return state;
+  }
+};
+
+export default articleReducer;
